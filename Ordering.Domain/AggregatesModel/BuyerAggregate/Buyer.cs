68c7c372bbc5538ff6a7ba5b603cf6cbb2d5bcc4
MehdiNikbakht <mahdinikbakht7105@gmail.com>
2022-11-14 20:36:38 +0330

    Create domain layer

---
+++ Ordering.Domain/AggregatesModel/BuyerAggregate/Buyer.cs
@@ -0,0 +1,24 @@
+﻿using Ordering.Domain.SeedWork;
+
+namespace Ordering.Domain.AggregatesModel.BuyerAggregate
+{
+    public class Buyer : Entity, IAggregateRoot
+    {
+        public string IdentityGuid { get; private set; }
+
+        public string Name { get; private set; }
+
+        protected Buyer()
+        {
+
+        }
+
+        public Buyer(string identity, string name) : this()
+        {
+            IdentityGuid = !string.IsNullOrWhiteSpace(identity) ? identity : throw new ArgumentNullException(nameof(identity));
+
+            Name = !string.IsNullOrWhiteSpace(name) ? name : throw new ArgumentNullException(nameof(name));
+        }
+
+    }
+}
---
+++ Ordering.Domain/AggregatesModel/OrderAggregate/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using Ordering.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.AggregatesModel.OrderAggregate
+{
+    public interface IOrderRepository
+    {
+        IUnitOfWork UnitOfWork { get; }
+        Order Add(Order order);
+        void Update(Order order);
+        Task<Order> GetAsync(int orderId);
+    }
+}
