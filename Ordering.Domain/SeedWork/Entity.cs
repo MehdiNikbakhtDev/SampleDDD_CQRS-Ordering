8ed75b2910acf764823d953a87e187eb46a0f68f
MehdiNikbakht <mahdinikbakht7105@gmail.com>
2022-11-14 20:34:29 +0330

    Add project files.

---
+++ Ordering.Domain/SeedWork/Entity.cs
@@ -0,0 +1,44 @@
+﻿using MediatR;
+namespace Ordering.Domain.SeedWork
+{
+    public abstract class Entity
+    {
+        int _Id;
+        public virtual int Id
+        {
+            get
+            {
+                return _Id;
+            }
+            protected set
+            {
+                _Id = value;
+            }
+        }
+
+        public bool IsTransient()
+        {
+            return this.Id == default(Int32);
+        }
+
+        private List<INotification> _domainEvents;
+        public IReadOnlyCollection<INotification> DomainEvents => _domainEvents?.AsReadOnly();
+
+        public void AddDomainEvent(INotification eventItem)
+        {
+            _domainEvents = _domainEvents ?? new List<INotification>();
+            _domainEvents.Add(eventItem);
+        }
+
+        public void RemoveDomainEvent(INotification eventItem)
+        {
+            _domainEvents?.Remove(eventItem);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents?.Clear();
+        }
+
+    }
+}
